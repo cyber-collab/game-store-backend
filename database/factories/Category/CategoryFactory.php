d7fb07cdf7f7defb62f931eba083ad829941262a
jacob <jacobshestel@gmail.com>
2024-02-21 14:19:06 +0100

    feat: add category, add sub-category

---
+++ database/factories/Category/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\Category;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>fake()->name(),
+            'status'=> rand(0, 1),
+            'slug'=>fake()->name()
+        ];
+    }
+}
