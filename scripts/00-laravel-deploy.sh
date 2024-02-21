4054d3afbe294a71f3909dd0af004959cfd86fab
Den <denyszkramar@gmail.com>
2024-02-21 18:28:44 +0200

    fix:add comand refresh for update table

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -10,6 +10,9 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
+php artisan migrate:refresh --force
+
+echo "Running migrations..."
 php artisan migrate --force
 
 echo "Seeding database..."
