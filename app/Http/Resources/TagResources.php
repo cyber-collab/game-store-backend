3adeddb18381faf168052aeb03433e08a2e5368b
Den <denyszkramar@gmail.com>
2024-04-15 16:42:41 +0300

    feat: add block hero

---
+++ app/Http/Resources/TagResources.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResources extends JsonResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'tag' => $this->tag
+        ];
+    }
+}
